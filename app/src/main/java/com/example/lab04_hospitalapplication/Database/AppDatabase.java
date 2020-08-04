250051e375c5a030a369e09b848b14cdfc61a5f3
hanguyen <https://github.com/hanguy2806>
2020-08-04 02:10:53 -0400

    Aug 4 -complete login screen

--- app/src/main/java/com/example/lab04_hospitalapplication/Database/AppDatabase.java
+++ app/src/main/java/com/example/lab04_hospitalapplication/Database/AppDatabase.java
@@ -1,10 +1,13 @@
 package com.example.lab04_hospitalapplication.Database;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.example.lab04_hospitalapplication.Dao.NurseDao;
 import com.example.lab04_hospitalapplication.Dao.PatientDao;
@@ -27,8 +30,32 @@ public abstract class AppDatabase extends RoomDatabase {
 
     public static synchronized AppDatabase getInstance(Context context) {
         if (INSTANCE == null) {
-            INSTANCE = Room.databaseBuilder(context, AppDatabase.class, DATABASE_NAME).build();
+            INSTANCE = Room.databaseBuilder(context, AppDatabase.class, DATABASE_NAME)
+//                    .fallbackToDestructiveMigration()
+//                    .addCallback(roomcallback)
+                    .build();
         }
         return INSTANCE;
     }
+
+    //populate database
+//    private static RoomDatabase.Callback roomcallback=new RoomDatabase.Callback(){
+//        @Override
+//        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+//            super.onCreate(db);
+//            new PopulateDbAsyncTask(INSTANCE).execute();
+//        }
+//    };
+//    private static class PopulateDbAsyncTask extends AsyncTask<Void,Void,Void> {
+//        private PatientDao patientDao;
+//        private PopulateDbAsyncTask(AppDatabase db){
+//            patientDao=db.patientDao();
+//        }
+//        @Override
+//        protected Void doInBackground(Void... voids) {
+//            patientDao.insert(new Patient("Title 1", "Decription 1","LaLaLa1",1));
+//            patientDao.insert(new Patient("Title 2", "Decription 2","LaLaLa2",2));
+//            return null;
+//        }
+//    }
 }
